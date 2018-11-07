6a02bc02b73f5c8d608fe3846a72ed2ce57db7de
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-07 15:53:04 -0800

    config for deploying

--- bitter/settings.py
+++ bitter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -149,3 +150,5 @@ REST_FRAMEWORK = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
