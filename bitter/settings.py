8e100d22e8b713b2df54faffcc4f08927ff078c2
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-08 10:41:15 -0800

    remove print test statement and change environ var for debug to empty string

--- bitter/settings.py
+++ bitter/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
-print("\n\n\n\n\n\n", bool(os.environ.get("DJANGO_DEBUG")), "\n\n\n\n\n")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
