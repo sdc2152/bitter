922c9410684d6dbf95672f5091dec40bb0529cff
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-07 18:26:57 -0800

    test

--- bitter/settings.py
+++ bitter/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 print(BASE_DIR)
-print(os.listdir(BASE_DIR))
+print(os.listdir(os.path.join(BASE_DIR, "frontend")))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend"),
 ]
