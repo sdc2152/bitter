2b860b2608b67be9ceebc82d31ad60a3754a6d3b
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-07 17:59:18 -0800

    test

--- bitter/settings.py
+++ bitter/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "frontend"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -64,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(FRONTEND_DIR, "build")
+            os.path.join(BASE_DIR, "frontend", "build")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -131,9 +130,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_DIRS = [
-    # os.path.join(FRONTEND_DIR, 'build', 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend", 'build', 'static'),
+]
 
 # Whitenoise
 
