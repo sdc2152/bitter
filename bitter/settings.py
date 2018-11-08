2d16559c993031b4e5274343a8177d8fedefec33
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-07 18:20:02 -0800

    test

--- bitter/settings.py
+++ bitter/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build'),
-]
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "build"),
+# ]
 
 # Whitenoise
 WHITENOISE_ROOT = os.path.join(BASE_DIR, "staticfiles")
