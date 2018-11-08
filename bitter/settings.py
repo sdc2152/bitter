e76b50c63d827f126a53ed127eea5f2f4af3cd55
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-07 18:31:38 -0800

    test

--- bitter/settings.py
+++ bitter/settings.py
@@ -128,10 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-print(BASE_DIR)
-print(os.listdir(os.path.join(BASE_DIR, "frontend")))
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend", "build"),
+    os.path.join(BASE_DIR, "frontend", "build", "static"),
 ]
 
 # Whitenoise
