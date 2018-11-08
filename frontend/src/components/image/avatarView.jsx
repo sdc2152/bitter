8ac268a4aad0eefda868f7b5b6465f0af0f388c7
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-11-08 12:52:00 -0800

    change alt in avatar image

--- frontend/src/components/image/avatarView.jsx
+++ frontend/src/components/image/avatarView.jsx
@@ -4,7 +4,7 @@ import DefaultAvatar from "../../images/default_avatar.jpg";
 const AvatarView = ({avatar, className}) => {
   const src = avatar ? avatar.image : DefaultAvatar;
   return (
-    <img className={className} src={src} alt={DefaultAvatar}>
+    <img className={className} src={src} alt="">
     </img>
   );
 };
